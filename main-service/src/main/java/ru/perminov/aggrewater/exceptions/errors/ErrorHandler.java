e39d36326c0f3d873695c7fef3750ed4595feb16
Андрей <Perminand@yandex.ru>
2024-09-28 08:15:29 +0300

    feat: Сделать ErrorHandler Дубликате ошибка

--- main-service/src/main/java/ru/perminov/aggrewater/exceptions/errors/ErrorHandler.java
+++ main-service/src/main/java/ru/perminov/aggrewater/exceptions/errors/ErrorHandler.java
@@ -1,10 +1,17 @@
 package ru.perminov.aggrewater.exceptions.errors;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.perminov.aggrewater.exceptions.ErrorResponse;
 
 @RestControllerAdvice
 public class ErrorHandler {
 
-    public ErrorResponse handleDublicate ()
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleDublicate(final DuplicatedDataException e) {
+        return new ErrorResponse(e.getMessage());
+    }
 }
